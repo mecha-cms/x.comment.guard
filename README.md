8fe3c2ae4d58181f1755c0f0a3396e29c9125c77
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-11-25 11:20:07 +0700

    Allow Valid E-Mail Provider(s) Only

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Comment Guard Extension for [Mecha](https://github.com/mecha-cms/mecha)
 Release Notes
 -------------
 
+### 1.1.0
+
+ - Added list of popular email host name to validate the email address so that any random but valid email address pattern such as `asdf@asdf` can be rejected.
+
 ### 1.0.2
 
  - [@mecha-cms/mecha#96](https://github.com/mecha-cms/mecha/issues/96)
--- comment.guard/index.php
+++ comment.guard/index.php
@@ -8,7 +8,10 @@ function comment__guard($any) {
         $content = $lot['content'] ?? "";
         $email = $lot['email'] ?? "";
         if ($content) {
-            if (
+            /* if (\preg_match('/[ЁёА-я]{2,}/', $content)) {
+                ++$error;
+                \Alert::error('Sorry for being racist. You can use regular alphabet to prevent this alert.');
+            } else */ if (
                 false !== \strpos($content, '://') &&
                 \preg_match_all('/\bhttps?:\/\/\S+/', $content, $m)
             ) {
@@ -16,27 +19,111 @@ function comment__guard($any) {
                     ++$error;
                     \Alert::error('Too many links in the comment.');
                 }
-            }
-            foreach (\stream(__DIR__ . \DS . 'words.txt') as $v) {
-                if ("" === ($v = \trim($v))) {
-                    continue;
-                }
-                if (false !== \stripos($content, $v)) {
-                    ++$error;
-                    \Alert::error('Please choose another word: %s', ['<mark>' . \htmlspecialchars($v) . '</mark>']);
-                    break;
+            } else {
+                foreach (\stream(__DIR__ . \DS . 'words.txt') as $v) {
+                    if ("" === ($v = \trim($v))) {
+                        continue;
+                    }
+                    if (false !== \stripos($content, $v)) {
+                        ++$error;
+                        \Alert::error('Please choose another word: %s', ['<mark>' . \htmlspecialchars($v) . '</mark>']);
+                        break;
+                    }
                 }
             }
         }
         if ($email) {
-            foreach (\stream(__DIR__ . \DS . 'email.txt') as $v) {
-                if ("" === ($v = \trim($v))) {
-                    continue;
-                }
-                if ($v === $email) {
-                    ++$error;
-                    \Alert::error('Blocked email address: %s', ['<mark>' . $v . '</mark>']);
-                    break;
+            $host = \strtok(\explode('@', $email, 2)[1] ?? "", '.');
+            // <https://email-verify.my-addr.com/list-of-most-popular-email-domains.php>
+            // <https://email-verify.my-addr.com/top-email-service-providers.php>
+            $hosts = [
+                'aim' => 1,
+                'alice' => 1,
+                'aliceadsl' => 1,
+                'aol' => 1,
+                'arcor' => 1,
+                'att' => 1,
+                'bellsouth' => 1,
+                'bigpond' => 1,
+                'bluewin' => 1,
+                'blueyonder' => 1,
+                'bol' => 1,
+                'centurytel' => 1,
+                'charter' => 1,
+                'chello' => 1,
+                'club-internet' => 1,
+                'comcast' => 1,
+                'cox' => 1,
+                'earthlink' => 1,
+                'facebook' => 1,
+                'free' => 1,
+                'freenet' => 1,
+                'frontiernet' => 1,
+                'gmail' => 1,
+                'gmx' => 1,
+                'googlemail' => 1,
+                'hello' => 1,
+                'hetnet' => 1,
+                'home' => 1,
+                'hotmail' => 1,
+                'ig' => 1,
+                'juno' => 1,
+                'laposte' => 1,
+                'libero' => 1,
+                'live' => 1,
+                'mac' => 1,
+                'mail' => 1,
+                'me' => 1,
+                'msn' => 1,
+                'neuf' => 1,
+                'ntlworld' => 1,
+                'optonline' => 1,
+                'optusnet' => 1,
+                'orange' => 1,
+                'outlook' => 1,
+                'planet' => 1,
+                'qq' => 1,
+                'rambler' => 1,
+                'rediff' => 1,
+                'rediffmail' => 1,
+                'rocketmail' => 1,
+                'sbcglobal' => 1,
+                'sfr' => 1,
+                'shaw' => 1,
+                'sky' => 1,
+                'skynet' => 1,
+                'sympatico' => 1,
+                't-online' => 1,
+                'telenet' => 1,
+                'terra' => 1,
+                'tin' => 1,
+                'tiscali' => 1,
+                'twcc' => 1,
+                'uol' => 1,
+                'verizon' => 1,
+                'virgilio' => 1,
+                'voila' => 1,
+                'wanadoo' => 1,
+                'web' => 1,
+                'windstream' => 1,
+                'yahoo' => 1,
+                'yandex' => 1,
+                'ymail' => 1,
+                'zonnet' => 1
+            ];
+            if (!isset($hosts[$host])) {
+                ++$error;
+                \Alert::error('Blocked email address: %s', ['<mark>' . $email . '</mark>']);
+            } else {
+                foreach (\stream(__DIR__ . \DS . 'email.txt') as $v) {
+                    if ("" === ($v = \trim($v))) {
+                        continue;
+                    }
+                    if ($v === $email) {
+                        ++$error;
+                        \Alert::error('Blocked email address: %s', ['<mark>' . $v . '</mark>']);
+                        break;
+                    }
                 }
             }
         }
