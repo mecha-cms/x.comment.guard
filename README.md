944edfff9b24ea6f0ac65640e2993dae1907141e
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-08-27 23:47:47 +0700

    Updated for Mecha 3.x.x

--- README.md
+++ README.md
@@ -4,20 +4,3 @@ Comment Guard Extension for [Mecha](https://github.com/mecha-cms/mecha)
 ![Code Size](https://img.shields.io/github/languages/code-size/mecha-cms/x.comment.guard?color=%23444&style=for-the-badge)
 
 This extension prevents users from publishing a comment based on certain criteria. For example, preventing users from writing more than five links in comments, blocking users coming from certain IP or email addresses, preventing users from writing certain words or sentences in comments, etc.
-
----
-
-Release Notes
--------------
-
-### 1.1.0
-
- - Added list of popular email host name to validate the email address so that any random but valid email address pattern such as `asdf@asdf` can be rejected.
-
-### 1.0.2
-
- - [@mecha-cms/mecha#96](https://github.com/mecha-cms/mecha/issues/96)
-
-### 1.0.0
-
- - Initial release.
---
+++ index.php
@@ -0,0 +1,181 @@
+<?php namespace x;
+
+function comment__guard($content, $path, $query, $hash) {
+    if ('POST' !== $_SERVER['REQUEST_METHOD']) {
+        return $content;
+    }
+    $error = 0;
+    $lot = (array) ($_POST['comment'] ?? []);
+    $content = $lot['content'] ?? "";
+    $email = $lot['email'] ?? "";
+    $ip = \ip();
+    $link = $lot['link'] ?? "";
+    if ($content) {
+        $content = \strip_tags($content);
+        /* if (\preg_match('/[ЁёА-я]{2,}/', $content)) {
+            ++$error;
+            \class_exists("\\Alert") && \Alert::error('Sorry for being racist. You can use regular alphabet to prevent this alert.');
+        } else */ if (
+            false !== \strpos($content, '://') &&
+            \preg_match_all('/\bhttps?:\/\/\S+/', $content, $m)
+        ) {
+            // The maximum link(s) in comment is `5`
+            if (\count($m[0]) > 5) {
+                \class_exists("\\Alert") && \Alert::error('Too many links in the comment.');
+                ++$error;
+            }
+        } else {
+            foreach (\stream(__DIR__ . \D . 'content.txt') as $v) {
+                if ("" === ($v = \trim($v))) {
+                    continue;
+                }
+                if (false !== \stripos($content, $v)) {
+                    \class_exists("\\Alert") && \Alert::error('Please use other words: %s', ['<mark>' . \htmlspecialchars($v) . '</mark>']);
+                    ++$error;
+                    break;
+                }
+            }
+        }
+    }
+    if ($email) {
+        $host = \strtok(\explode('@', $email, 2)[1] ?? "", '.');
+        // <https://email-verify.my-addr.com/list-of-most-popular-email-domains.php>
+        // <https://email-verify.my-addr.com/top-email-service-providers.php>
+        $hosts = [
+            'aim' => 1,
+            'alice' => 1,
+            'aliceadsl' => 1,
+            'aol' => 1,
+            'arcor' => 1,
+            'att' => 1,
+            'bellsouth' => 1,
+            'bigpond' => 1,
+            'bluewin' => 1,
+            'blueyonder' => 1,
+            'bol' => 1,
+            'centurytel' => 1,
+            'charter' => 1,
+            'chello' => 1,
+            'club-internet' => 1,
+            'comcast' => 1,
+            'cox' => 1,
+            'earthlink' => 1,
+            'facebook' => 1,
+            'free' => 1,
+            'freenet' => 1,
+            'frontiernet' => 1,
+            'gmail' => 1,
+            'gmx' => 1,
+            'googlemail' => 1,
+            'hello' => 1,
+            'hetnet' => 1,
+            'home' => 1,
+            'hotmail' => 1,
+            'ig' => 1,
+            'juno' => 1,
+            'laposte' => 1,
+            'libero' => 1,
+            'live' => 1,
+            'mac' => 1,
+            'mail' => 1,
+            'me' => 1,
+            'msn' => 1,
+            'neuf' => 1,
+            'ntlworld' => 1,
+            'optonline' => 1,
+            'optusnet' => 1,
+            'orange' => 1,
+            'outlook' => 1,
+            'planet' => 1,
+            'qq' => 1,
+            'rambler' => 1,
+            'rediff' => 1,
+            'rediffmail' => 1,
+            'rocketmail' => 1,
+            'sbcglobal' => 1,
+            'sfr' => 1,
+            'shaw' => 1,
+            'sky' => 1,
+            'skynet' => 1,
+            'sympatico' => 1,
+            't-online' => 1,
+            'telenet' => 1,
+            'terra' => 1,
+            'tin' => 1,
+            'tiscali' => 1,
+            'twcc' => 1,
+            'uol' => 1,
+            'verizon' => 1,
+            'virgilio' => 1,
+            'voila' => 1,
+            'wanadoo' => 1,
+            'web' => 1,
+            'windstream' => 1,
+            'yahoo' => 1,
+            'yandex' => 1,
+            'ymail' => 1,
+            'zonnet' => 1
+        ];
+        if (!isset($hosts[$host])) {
+            \Alert::error('Blocked email address: %s', ['<mark>' . $email . '</mark>']);
+            ++$error;
+        } else {
+            foreach (\stream(__DIR__ . \D . 'email.txt') as $v) {
+                if ("" === ($v = \trim($v))) {
+                    continue;
+                }
+                if ($v === $email) {
+                    \Alert::error('Blocked email address: %s', ['<mark>' . $v . '</mark>']);
+                    ++$error;
+                    break;
+                }
+            }
+        }
+    }
+    if ($ip) {
+        foreach (\stream(__DIR__ . \D . 'ip.txt') as $v) {
+            if ("" === ($v = \trim($v))) {
+                continue;
+            }
+            if ($v === $ip) {
+                \class_exists("\\Alert") && \Alert::error('Blocked internet protocol address: %s', ['<mark>' . $v . '</mark>']);
+                ++$error;
+                break;
+            }
+        }
+    }
+    // Make sure link value is in the form of base URL
+    if ($link) {
+        if (
+            // `http://example.com?a=b`
+            false !== \strpos($link, '?') ||
+            // `http://example.com&a=b`
+            false !== \strpos($link, '&') ||
+            // `http://example.com#a`
+            false !== \strpos($link, '#') ||
+            // `http://example.com/a`
+            \substr_count(\rtrim($link, '/'), '/') > 2
+        ) {
+            \class_exists("\\Alert") && \Alert::error('Link must be in the form of base URL. Links that point to a specific page is not allowed.');
+            ++$error;
+        }
+        foreach (\stream(__DIR__ . \D . 'link.txt') as $v) {
+            if ("" === ($v = \trim($v))) {
+                continue;
+            }
+            if ($v === $link) {
+                \class_exists("\\Alert") && \Alert::error('Blocked link address: %s', ['<mark>' . $v . '</mark>']);
+                ++$error;
+                break;
+            }
+        }
+    }
+    if ($error > 0) {
+        unset($lot['token']);
+        $_SESSION['form']['comment'] = $lot;
+        \kick($path . $query . ($hash ?? '#comment'));
+    }
+    return $content;
+}
+
+\Hook::set('route.comment', __NAMESPACE__ . "\\comment__guard", 0);
